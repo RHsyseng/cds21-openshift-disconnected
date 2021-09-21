43419ffaf62150c212c0e9e2a271787aa20a9c4f
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2021-09-21 08:14:45 +0200

    better view on cert-maker

--- assets/src/cert-maker.sh
+++ assets/src/cert-maker.sh
@@ -11,9 +11,9 @@ cert_ou="IT"           # Certificate Organizational Unit (OU)
 cert_cn="${host_fqdn}" # Certificate Common Name (CN)
 mkdir -p ${path}/{auth,certs,data}
 
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout ${path}/certs/domain.key -x509 -days 3650 -out ${path}/certs/domain.crt -addext "subjectAltName = DNS:${host_fqdn}"
--subj "/C=${cert_c}/ST=${cert_s}/L=${cert_l}/O=${cert_o}/OU=${cert_ou}/CN=${cert_cn}"
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout ${path}/certs/domain.key -x509 -days 3650 \
+    -out ${path}/certs/domain.crt -addext "subjectAltName = DNS:${host_fqdn}" \
+    -subj "/C=${cert_c}/ST=${cert_s}/L=${cert_l}/O=${cert_o}/OU=${cert_ou}/CN=${cert_cn}"
 
-sudo cp ${path}/certs/domain.crt /etc/pki/ca-trust/source/anchors/
-sudo update-ca-trust extract
+sudo cp ${path}/certs/domain.crt /etc/pki/ca-trust/source/anchors/ && sudo update-ca-trust extract
 htpasswd -bBc ${path}/auth/htpasswd dummy dummy
